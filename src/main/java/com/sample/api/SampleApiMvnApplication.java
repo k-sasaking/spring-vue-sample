c9d2961bbe073b11293b9335a7495ecc20b352c7
kazuya.sasaki <kazuya.sasaki39@gmail.com>
2020-07-25 23:01:04 +0900

    tomcat用にmainを変更

--- src/main/java/com/sample/api/SampleApiMvnApplication.java
+++ src/main/java/com/sample/api/SampleApiMvnApplication.java
@@ -2,12 +2,20 @@ package com.sample.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleApiMvnApplication {
+public class SampleApiMvnApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApiMvnApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleApiMvnApplication.class);
+    }
+
 
 }
